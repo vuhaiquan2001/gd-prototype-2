3581790641efe1df8dba773a1e7ce9dc473673ba
vuhaiquan2001 <ohmychuoi454545@gmail.com>
2025-09-23 23:03:51 +0700

    .

--- Combat/CombatManager.gd
+++ Combat/CombatManager.gd
@@ -1,7 +1,7 @@
 extends Node2D
 # Preload actor scene để tối ưu performance 
 @onready var actor_scene = preload("res://Combat/Actor.tscn") 
-@onready var background = preload("res://Combat/Background.tscn") 
+@onready var parallax_layer_scene = preload("res://Combat/ParallaxLayer.tscn") 
 
 func _ready() -> void:
 	load_background()
@@ -28,9 +28,39 @@ func spawn_actor(monster, index) -> void:
 		print("Actor scene not found")
 
 func load_background() -> void:
-	if background:
-		print("Load")
-		var bg = background.instantiate()
-		add_child(bg)
+	var json_data = FileAccess.open("res://Data/map.json", FileAccess.READ)
+	if json_data:
+		var map_data = JSON.parse_string(json_data.get_as_text())
+		if typeof(map_data) == TYPE_DICTIONARY:
+			for key in map_data:
+				# Tạo instance của 1 layer
+				var parallax: Parallax2D = parallax_layer_scene.instantiate()
+				var sprite: Sprite2D = parallax.get_child(0)
+				print(map_data[key])
+				# Gán texture từ đường dẫn trong json
+				var tex = load(map_data[key])
+				if tex:
+					sprite.texture = tex
+					# Full width screen sprite
+					fit_sprite_fullscreen(sprite)
+					# Tuỳ chỉnh sprite (fit màn hình chẳng hạn)
+					sprite.position = Vector2.ZERO
+					sprite.scale = Vector2(1, 1) # tuỳ chỉnh cho vừa màn hình
+					
+					# Tuỳ chỉnh motion của parallax
+					parallax.scroll_scale = Vector2(0.5, 1) # layer này cuộn chậm hơn
+				# Thêm parallax layer vào node cha (giả sử self là ParallaxBackground)
+				add_child(parallax)
+		else:
+			print("map.json format is invalid")
 	else:
 		print("Background scene not found")
+		
+func fit_sprite_fullscreen(sprite: Sprite2D) -> void:
+	if sprite.texture:
+		var tex_size = sprite.texture.get_size()
+		var screen_size = get_viewport_rect().size
+		var scale_x = screen_size.x / tex_size.x
+		var scale_y = screen_size.y / tex_size.y
+		sprite.scale = Vector2(scale_x, scale_y)
+		sprite.position = screen_size / 2   # căn giữa
